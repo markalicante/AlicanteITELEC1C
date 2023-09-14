4d74fdc732438a5ebffbf17f24c2cf25871c8aa8
MARK <MARK@MARK>
2023-09-14 21:52:33 +0800

    Lab Act

---
+++ AlicanteITELEC1C/Controllers/InstructorController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AlicanteITELEC1C.Models;
+
+namespace AlicanteITELEC1C.Controllers
+{
+    public class InstructorController : Controller
+    {
+        List<Instructor> InstructorList = new List<Instructor>()
+        {
+            new Instructor()
+            {
+                InstructorId = 1, InsFirst = "Gabriel", InsLast = "Montano", HiringDate = DateTime.Parse("2022-08-26"), IsTenured = false, InstructorRank = Rank.Instructor
+            },
+            new Instructor()
+            {
+                InstructorId = 2, InsFirst = "Leo", InsLast = "Lintag", HiringDate = DateTime.Parse("2022-08-07"), IsTenured = true, InstructorRank = Rank.Professor
+            },
+            new Instructor()
+            {
+                InstructorId = 3, InsFirst = "Eugenia", InsLast = "Zhuo", HiringDate = DateTime.Parse("2020-01-25"), IsTenured = true, InstructorRank = Rank.AssociateProfessor
+            }
+
+        };
+
+        public IActionResult Index()
+        {
+
+            return View(InstructorList);
+        }
+
+        public IActionResult ShowDetail(int id)
+        {
+            Instructor? instructor = InstructorList.FirstOrDefault(ins => ins.InstructorId == id);
+
+            if (instructor != null)
+                return View(instructor);
+
+            return NotFound();
+        }
+
+        [HttpGet]
+        public IActionResult AddInstructor()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddInstructor(Instructor newInstructor)
+        {
+            InstructorList.Add(newInstructor);
+            return View("Index", InstructorList);
+        }
+    }
+}
---
+++ AlicanteITELEC1C/Models/Instructor.cs
@@ -0,0 +1,22 @@
+﻿namespace AlicanteITELEC1C.Models
+{
+    public enum Rank
+    {
+        Instructor, AssistantProfessor, AssociateProfessor, Professor
+    }
+    public class Instructor
+    {
+        public int InstructorId { get; set; }
+
+        public string InsFirst { get; set; }
+
+        public string InsLast { get; set; }
+
+        public Boolean IsTenured { get; set; }
+
+        public Rank InstructorRank { get; set; }
+
+        public DateTime HiringDate { get; set; }
+
+    }
+}
